b100b11c21a32b086dc7bcff51a01dbdba6b4054
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-06-08 19:49:50 +0600

    Create member.js

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember(){
+  return{
+    restrict: 'E',
+    templateUrl: 'modules/skills/views/member.html',
+    controller: 'SkillsMemberController',
+    scope: {
+      member: 'm'
+    }
+  };
+}
